3dd55125d22fb9edc35a53c9bae9bc5c376ebd0b
Shannon Turner <shannon.v.turner@gmail.com>
2016-03-26 09:26:22 -0400

    Adjusted urls, Supporter.support_long

--- wearenotthis/urls.py
+++ wearenotthis/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from .views import HomeView
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('wearenotthis.urls'), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
